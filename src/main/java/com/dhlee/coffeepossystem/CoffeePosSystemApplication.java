c6a1a649856f6f0205b60202b05e5cea6893c2a3
donghui.lee <donghui.lee@bugs.co.kr>
2023-05-02 11:12:36 +0900

    [FEAT] 초기 애플리케이션 추가

---
+++ src/main/java/com/dhlee/coffeepossystem/CoffeePosSystemApplication.java
@@ -0,0 +1,19 @@
+package com.dhlee.coffeepossystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableCaching
+@EnableJpaAuditing
+@EnableScheduling
+@SpringBootApplication
+public class CoffeePosSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeePosSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhlee/coffeepossystem/CoffeePosSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhlee.coffeepossystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeePosSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
